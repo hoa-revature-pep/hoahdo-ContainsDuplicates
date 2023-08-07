16243434458dd18ef14b96c49001ae7ffd5a925e
Hoa Do <hoa.do.ma@gmail.com>
2023-08-07 21:26:29 +0000

    Completed Challenge: Contains Duplicate

--- src/main/java/ContainsDuplicates.java
+++ src/main/java/ContainsDuplicates.java
@@ -1,4 +1,4 @@
-
+import java.util.HashSet;
 
 public class ContainsDuplicates {
     /**
@@ -8,6 +8,16 @@ public class ContainsDuplicates {
      * @return true if nums contains any duplicate values, false if it does not.
      */
     public boolean containsDuplicate(int[] nums){
+
+        HashSet<Integer> hSet = new HashSet<>();
+
+        for (int idx = 0; idx < nums.length; idx++) {
+            if (hSet.contains(nums[idx])) {
+                return true;
+            }
+            hSet.add(nums[idx]);
+        }
+
         return false;
     }
 }
